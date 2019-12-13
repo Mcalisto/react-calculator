7de5a87fdcf6913af886f973f643039f80103403
mcalisto <mcalisto@email.com>
2019-12-12 22:01:03 -0200

    Calculator layout using css.

---
+++ src/calculator.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import './calculator.css'
+import Display from './display'
+import Button from './button'
+
+
+export default class Calculator extends Component {
+  render() {
+    return(
+      <div className="calculator">
+
+      <Display value="456465"/>
+
+        <Button label='AC'/>
+        <Button label='/'/>
+        <Button label='7'/>
+        <Button label='8'/>
+        <Button label='9'/>
+        <Button label='4'/>
+        <Button label='5'/>
+        <Button label='6'/>
+        <Button label='-'/>
+        <Button label='1'/>
+        <Button label='2'/>
+        <Button label='3'/>
+        <Button label='+'/>
+        <Button label='0'/>
+        <Button label='.'/>
+        <Button label='='/>
+
+
+      </div>
+    )
+  }
+}
